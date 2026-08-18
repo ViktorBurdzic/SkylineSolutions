07c03270ab51663bc896ef6832cd3ccd75747fe0
Viktor Burdžić <vburdzic@gmail.com>
2026-08-18 09:07:36 +0200

    Update site URL to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://skylinesolutions.skylinesolutionsai.workers.dev',
+  site: 'https://skylinesolutions.dev',
   integrations: [react(), sitemap()],
 
   vite: {
